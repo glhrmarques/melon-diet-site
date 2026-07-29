d09df60eb452898c138afb6e49c91f716316e6e5
glhrmarques <gmarques.pd@gmail.com>
2026-07-29 03:23:34 -0300

    content

--- src/components/card/HomeCard.tsx
+++ src/components/card/HomeCard.tsx
@@ -7,10 +7,21 @@ export default function HomeCard({
 }: HomeCardProps) {
   return (
     <div
-      className="h-[400px] w-[400px] rounded-[16px] p-component-default"
+      className="
+      flex flex-col gap-10
+      h-[400px] w-[400px] rounded-[16px] p-[40px]"
       style={{ backgroundColor }}
     >
-
+      <div className="
+      flex flex-row justify-between items-center">
+        <p className="font-black text-[48px] leading-none">
+          7:00
+        </p>
+        <p className="font-medium text-[18px] leading-none">
+          Café da manhã
+        </p>
+      </div>
+      <p> Ovo de galinha refogado (2 unidades)</p>
     </div>
   );
 }
